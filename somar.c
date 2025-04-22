8eaa1804e881de590abdd952543801077a1279e9
Diego Fernandes <161744701+didifernandes@users.noreply.github.com>
2025-04-22 20:26:57 -0300

    Update somar.c

--- somar.c
+++ somar.c
@@ -3,11 +3,13 @@
 int main() {
   int a;
   int b;
+  
 int soma;
-printf("digite um numero inteiro:  ");
+  printf("digite um numero inteiro: ");
 scanf("%d", &a);
-printf("digite outro numero inteiro:  ");
+  printf("digite outro numero inteiro:  ");
 scanf("%d", &b);
-soma = a + b;
+  soma = a + b;
 printf("%d", soma);
+    return 0;
 }
