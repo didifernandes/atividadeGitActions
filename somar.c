a6b4fd33e4b149bb16d1bfc6af3b3dab52d9837d
Diego Fernandes <161744701+didifernandes@users.noreply.github.com>
2025-04-22 20:26:05 -0300

    Create somar.c

---
+++ somar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+  int a;
+  int b;
+int soma;
+printf("digite um numero inteiro:  ");
+scanf("%d", &a);
+printf("digite outro numero inteiro:  ");
+scanf("%d", &b);
+soma = a + b;
+printf("%d", soma);
+}
